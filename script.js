ba31d2d0cad7ff7210bce4ca458b3b72db4bb4c6
DomingosManuelFranco <manuelfranco070@gmail.com>
2021-03-10 06:34:22 +0100

    finished the 16x16 grid via javascript

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 
-const square = document.getElementById("square");
+const container = document.getElementById("container");
 
 function makeRows(rows, cols) {
+
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
     cell.innerText = (c + 1);
-    square.appendChild(cell).className = "grid-item";
-  };
+    container.appendChild(cell).className = "grid-item";
+  }
 }
 
 makeRows(16, 16);
